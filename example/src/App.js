d58c1437439a049d280f7e8b8452ed7be8d0572c
Harsh Darji <harshdarji1995@gmail.com>
2020-07-08 19:51:20 +0530

    update

--- example/src/App.js
+++ example/src/App.js
@@ -119,4 +119,21 @@ const App = () => {
 	)
 }
 
-export default App
+const App2 = () => {
+	const [hour24, setHour] = useState(22)
+  const [minute, setMin] = useState(30)
+  // OR use hour (12 hour format), minute and meridian (AM | PM) for props
+  // OR for string time use : "HH:mm" ( 24 hrs ) | "hh:mm aa" ( 12 hrs )
+
+  const handleTimeUpdate = ({time}) => {
+    setHour(time.hour24)
+    setMin(time.minute)
+  }
+
+  return (
+    <TimePicker 
+      onTimeUpdate={handleTimeUpdate} />
+  )
+}
+
+export default App2
