ce67e5dfcaddce0e43209ffb6f469508bb593e69
Dishant Chavda <dishantchavda@gmail.com>
2020-09-27 10:05:58 +0530

    Updates

---
+++ precache-manifest.a59b941cab05d069c30bea7139a9d879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a8166c0d311a284738d0390931fcfa",
+    "url": "/react-datetime-range-super-picker/index.html"
+  },
+  {
+    "revision": "37788b35a61f39f2b1aa",
+    "url": "/react-datetime-range-super-picker/static/css/main.f703a8a7.chunk.css"
+  },
+  {
+    "revision": "d607ef0ac7b439b25489",
+    "url": "/react-datetime-range-super-picker/static/js/2.6f2a955b.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/react-datetime-range-super-picker/static/js/2.6f2a955b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37788b35a61f39f2b1aa",
+    "url": "/react-datetime-range-super-picker/static/js/main.94286b7b.chunk.js"
+  },
+  {
+    "revision": "a8ed8de4a17a35664d97",
+    "url": "/react-datetime-range-super-picker/static/js/runtime-main.e68ab877.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-datetime-range-super-picker/precache-manifest.57c94e5f0e23f112b8722180e66f4ed2.js"
+  "/react-datetime-range-super-picker/precache-manifest.a59b941cab05d069c30bea7139a9d879.js"
 );
 
 self.addEventListener('message', (event) => {
