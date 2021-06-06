2c198ae4918faa702059e687710274fe29451de0
Dishant Chavda <dishantchavda@gmail.com>
2021-06-06 18:47:32 +0530

    UPDATE: Minor props update

--- src/utils/style.utils.tsx
+++ src/utils/style.utils.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import { assign } from 'lodash'
 import { getThemeColors } from './style.theme'
+// TypeScript imports
 import { ComponentTheme } from '../interfaces/style.interfaces'
+import {
+  TimePickerInputProps, TimePickerProps
+} from '../interfaces/timepicker.interfaces'
+import { MonthPickerProps } from '../interfaces/monthpicker.interfaces'
+import {
+  DatePickerInputProps, DatePickerProps
+} from '../interfaces/datepicker.interfaces'
+import {
+  DateTimePickerInputProps,
+  DateTimePickerProps
+} from '../interfaces/datetimepicker.interfaces'
+import {
+  DateRangePickerInputProps, DateRangePickerProps, 
+  RangePickerInputProps, RangePickerProps
+} from '../interfaces/rangepicker.interfaces'
 
+
+type WrapperProps = 
+  TimePickerInputProps |
+  TimePickerProps |
+  MonthPickerProps |
+  DatePickerProps |
+  DatePickerInputProps |
+  DateTimePickerProps |
+  DateTimePickerInputProps |
+  RangePickerProps |
+  RangePickerInputProps |
+  DateRangePickerProps |
+  DateRangePickerInputProps
 
 /**
  * Wrapper component to all exported library components
@@ -10,7 +39,7 @@ import { ComponentTheme } from '../interfaces/style.interfaces'
  * Merge theme colors with prop colors or default colors
  * and create colors object to be used by all component
  */
- const StyleWrapper = <P extends object>(
+ const StyleWrapper = <P extends WrapperProps>(
   WrappedComponent: React.ComponentType<P>
 ) => {
 	return class ComponentWrapper extends React.Component<
