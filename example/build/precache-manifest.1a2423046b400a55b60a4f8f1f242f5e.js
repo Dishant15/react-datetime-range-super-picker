d560a11ba53841639a956ab0c0757226b0f297e3
Dishant Chavda <dishantchavda@gmail.com>
2021-06-26 22:28:21 +0530

    deploy

--- example/build/precache-manifest.1a2423046b400a55b60a4f8f1f242f5e.js
+++ example/build/precache-manifest.1a2423046b400a55b60a4f8f1f242f5e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7c19a8c9c9f036e50d5230997a3c07c6",
+    "revision": "a88f485f45d112c4a4f209ef0af3da86",
     "url": "/react-datetime-range-super-picker/index.html"
   },
   {
-    "revision": "94dd9079bc06ed3fa7cb",
+    "revision": "976130d209a86dc4ec2b",
     "url": "/react-datetime-range-super-picker/static/css/main.ba0e79f2.chunk.css"
   },
   {
-    "revision": "d298c4e2bf0ca7de9720",
-    "url": "/react-datetime-range-super-picker/static/js/2.8647deaa.chunk.js"
+    "revision": "2568cfadc0227b5b4d1d",
+    "url": "/react-datetime-range-super-picker/static/js/2.60e1af46.chunk.js"
   },
   {
     "revision": "90565ba0b43e1b95d2876295dfd7014d",
-    "url": "/react-datetime-range-super-picker/static/js/2.8647deaa.chunk.js.LICENSE.txt"
+    "url": "/react-datetime-range-super-picker/static/js/2.60e1af46.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "94dd9079bc06ed3fa7cb",
-    "url": "/react-datetime-range-super-picker/static/js/main.389d617a.chunk.js"
+    "revision": "976130d209a86dc4ec2b",
+    "url": "/react-datetime-range-super-picker/static/js/main.6e2d41ae.chunk.js"
   },
   {
     "revision": "a8ed8de4a17a35664d97",
